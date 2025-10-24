7cc487de0472e2ec2e5ecabe184f465f2fe1b6d1
Perkz Zheng <67892460+PerkzZheng@users.noreply.github.com>
2025-10-24 08:21:04 +0000

    update trtllm-gen sparse mla kernels
    Signed-off-by: Perkz Zheng <67892460+PerkzZheng@users.noreply.github.com>

--- cpp/tensorrt_llm/kernels/trtllmGenKernels/fmha/cubin/FmhaSm100fKernel_QkvBfloat16OBfloat16HQk576HV512HVPerCta256PagedKvDenseP4VarSeqSparseQ64Kv128Static2CtaKeepsAbForGen_cubin.cpp
+++ cpp/tensorrt_llm/kernels/trtllmGenKernels/fmha/cubin/FmhaSm100fKernel_QkvBfloat16OBfloat16HQk576HV512HVPerCta256PagedKvDenseP4VarSeqSparseQ64Kv128Static2CtaKeepsAbForGen_cubin.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa440a6e4cffb45f7251deac63663615f0fc7f5488ceb5d4d4620cee91eb2ac5
-size 967561
+oid sha256:ad8b637ea9323b01b8b269578321e12d4436cc55bc0d31a5a32a8b8774a59457
+size 966477
--- cpp/tensorrt_llm/kernels/trtllmGenKernels/fmha/cubin/FmhaSm100fKernel_QkvE4m3OBfloat16HQk576HV512HVPerCta256PagedKvDenseP4MultiCtasKvGmemSepVarSeqSparseQ64Kv128Static2CtaKeepsAbForGen_cubin.cpp
+++ cpp/tensorrt_llm/kernels/trtllmGenKernels/fmha/cubin/FmhaSm100fKernel_QkvE4m3OBfloat16HQk576HV512HVPerCta256PagedKvDenseP4MultiCtasKvGmemSepVarSeqSparseQ64Kv128Static2CtaKeepsAbForGen_cubin.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5aa61219f5a644aac69b01103f5040c48a4919a72a72cb7a0e506d07ec46ead4
+oid sha256:01e5a99864618a69b5037f85e4b5bbc3b8a9284a8ea770cc4afae8cb46192444
 size 877901
