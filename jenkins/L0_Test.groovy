8b050dffcd743b45b65abe58fed7463254e44c6c
Fanrong Li <lfr-0531@users.noreply.github.com>
2026-05-29 08:52:59 +0000

    [None][infra] Restore package sanity check stages
    Signed-off-by: Fanrong Li <lfr-0531@users.noreply.github.com>

--- jenkins/L0_Test.groovy
+++ jenkins/L0_Test.groovy
@@ -3419,9 +3419,57 @@ def launchTestJobs(pipeline, testFilter)
     }]]}
 
     // Python version and OS for sanity check
-    x86SanityCheckConfigs = [:]
+    // Slots: [buildImage, gpuType, cpuArch, reinstallDependencies, wheelPath, pipInstallImage, extraPytorchInstall]
+    x86SanityCheckConfigs = [
+        "PY312-DLFW": [
+            LLM_DOCKER_IMAGE,
+            "B200_PCIe",
+            X86_64_TRIPLE,
+            false,
+            "dlfw/",
+            DLFW_IMAGE,
+            false,
+        ],
+        "PY310-UB2204": [
+            LLM_ROCKYLINUX8_PY310_DOCKER_IMAGE,
+            "A10",
+            X86_64_TRIPLE,
+            true,
+            "",
+            UBUNTU_22_04_IMAGE,
+            true, // Extra install PyTorch CUDA 13.0 package to align with the CUDA version used for building TensorRT LLM wheels.
+        ],
+        "PY312-UB2404": [
+            LLM_ROCKYLINUX8_PY312_DOCKER_IMAGE,
+            "A100X",
+            X86_64_TRIPLE,
+            true,
+            "",
+            UBUNTU_24_04_IMAGE,
+            true, // Extra PyTorch CUDA 13.0 install
+        ],
+    ]
 
-    aarch64SanityCheckConfigs = [:]
+    aarch64SanityCheckConfigs = [
+        "PY312-UB2404": [
+            LLM_DOCKER_IMAGE,
+            "GH200",
+            AARCH64_TRIPLE,
+            false,
+            "",
+            UBUNTU_24_04_IMAGE,
+            true, // Extra PyTorch CUDA 13.0 install
+        ],
+        "PY312-DLFW": [
+            LLM_DOCKER_IMAGE,
+            "GH200",
+            AARCH64_TRIPLE,
+            false,
+            "dlfw/",
+            DLFW_IMAGE,
+            false,
+        ],
+    ]
 
     def toStageName = { gpuType, key -> "${gpuType}-PackageSanityCheck-${key}".toString() }
     fullSet += x86SanityCheckConfigs.collectEntries{ key, values -> [toStageName(values[1], key), null] }.keySet()
